6d8de6fff2b6c94189da7bd949d9aeb9c7016959
Maikel D <maikel.daloo@gmail.com>
2013-05-02 02:02:33 +1000

    Updated theme to load page content

--- wp-content/themes/sparky/page.php
+++ wp-content/themes/sparky/page.php
@@ -7,6 +7,12 @@
 		<section id="body">
 			<role id="content" role="main">
 				
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<section class="entry-content clearfix">
+						<?php the_content(); ?>
+					</section> <!-- end article section -->
+				<?php endwhile; endif; ?>
+				
 			</role>
 			
 			<?php partial('sidebar') ?>
