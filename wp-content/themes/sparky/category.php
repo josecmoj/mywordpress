3aeb3bc19d7750a53f539b06d9acabccda62dd98
Maikel D <maikel.daloo@gmail.com>
2013-05-19 12:06:56 +1000

    Added a category template which will handle the rendering for all category pages

---
+++ wp-content/themes/sparky/category.php
@@ -0,0 +1,26 @@
+<?php partial('head') ?>
+	
+<div id="container">
+	
+	<?php partial('header') ?>
+	
+	<section id="body">
+		<role id="content" role="main">
+			
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<section class="entry-content clearfix">
+					<?php the_content(); ?>
+				</section>
+			<?php endwhile; endif; ?>
+			
+		</role>
+		
+		<?php partial('sidebar') ?>
+		
+	</section>
+	
+	<?php partial('footer') ?>
+	
+</div>
+	
+<?php partial('foot'); ?>
