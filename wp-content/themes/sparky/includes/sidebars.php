25043f8f11b362f215e3437a513902021e2bcf7b
Maikel D <maikel.daloo@gmail.com>
2013-11-27 20:39:16 +1100

    Added a little snippet to allow us to unregister widgets

--- wp-content/themes/sparky/includes/sidebars.php
+++ wp-content/themes/sparky/includes/sidebars.php
@@ -21,3 +21,23 @@ if ( function_exists('register_sidebar') )
 		register_sidebar( $sidebar + $sidebar_defaults );
 	}
 }
+
+
+/**
+ * Unregister the widgets that we don't want the client to use.
+ */
+// add_action( 'widgets_init' , function() {
+// 	unregister_widget( 'WP_Widget_Pages' );
+// 	unregister_widget( 'WP_Widget_Calendar' );
+// 	unregister_widget( 'WP_Widget_Archives' );
+// 	unregister_widget( 'WP_Widget_Links' );
+// 	unregister_widget( 'WP_Widget_Meta' );
+// 	unregister_widget( 'WP_Widget_Search' );
+// 	unregister_widget( 'WP_Widget_Categories' );
+// 	unregister_widget( 'WP_Widget_Recent_Posts' );
+// 	unregister_widget( 'WP_Widget_Recent_Comments' );
+// 	unregister_widget( 'WP_Widget_RSS' );
+// 	unregister_widget( 'WP_Widget_Tag_Cloud' );
+// 	unregister_widget( 'WP_Nav_Menu_Widget' );
+// 	unregister_widget( 'WP_Widget_Text' );
+// });
