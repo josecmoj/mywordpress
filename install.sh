ba07ac5490cd1075170c4e573549cf3035f3ede8
Maikel D <maikel.daloo@gmail.com>
2014-01-04 14:38:42 +1100

    Removed the install script.. it's quite useless

--- install.sh
+++
@@ -1,5 +0,0 @@
-# Load the latest version of MyWordPress.
-curl -L https://github.com/maikeldaloo/mywordpress/archive/master.tar.gz | tar zx
-mv mywordpress-master/* mywordpress-master/.* .
-rm -rf mywordpress-master
-echo "Installed MyWordPress successfully."
