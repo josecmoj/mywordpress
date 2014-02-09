eabea29f339c4473f6a5954e3d5a069020226367
Maikel D <maikel.daloo@gmail.com>
2014-02-09 19:32:21 +1100

    Updated the package.json and gulp file to use a new watcher and add notifications

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp   = require( 'gulp' ),
-	gutil  = require( 'gulp-util' ),
 	concat = require( 'gulp-concat' ),
-	sass  = require( 'gulp-ruby-sass' ),
-	uglify = require( 'gulp-uglify' );
+	sass   = require( 'gulp-ruby-sass' ),
+	uglify = require( 'gulp-uglify' ),
+	notify = require( 'gulp-notify' ),
+	watchr = require( 'watchr' ),
+	path   = require( 'path' );
 
-var theme  = 'wp-content/themes/sparky/';
-var assets = theme + 'assets/';
+var theme  = 'wp-content/themes/sparky/',
+	assets = theme + 'assets/';
 
 gulp.task( 'styles' , function() {
 	gulp.src( assets + 'css/*.scss' )
@@ -16,28 +18,45 @@ gulp.task( 'styles' , function() {
 	gulp.src( assets + 'css/*.scss' )
 		.pipe( sass({ style: 'compressed' }) )
 		.pipe( concat( 'main.min.css' ) )
-		.pipe( gulp.dest( theme + 'css/' ) );
+		.pipe( gulp.dest( theme + 'css/' ) )
+		.pipe( notify({ title: 'Stylesheets Compiled' , message: 'Stylesheets have been compiled successfully.' }) );
 });
 
 gulp.task( 'scripts' , function() {
-	gulp.src([ assets + 'js/*' , assets + 'js/libs/**' , assets + 'js/modules/**' ])
+	gulp.src( assets + 'js/**/*.js' )
 		.pipe( concat( 'main.src.js' ) )
 		.pipe( gulp.dest( theme + 'js/' ) );
 	
-	gulp.src([ assets + 'js/*' , assets + 'js/libs/**' , assets + 'js/modules/**' ])
+	gulp.src( assets + 'js/**/*.js' )
 		.pipe( uglify() )
 		.pipe( concat( 'main.min.js' ) )
-		.pipe( gulp.dest( theme + 'js/' ) );
+		.pipe( gulp.dest( theme + 'js/' ) )
+		.pipe( notify({ title: 'JavaScript Compiled' , message: 'JavaScript files have been compiled successfully.' }) );
 });
 
-gulp.task( 'default' , function () {
-	gulp.run( 'styles' , 'scripts' );
+/**
+ * Use Watchr instead of the default gulp.watch because gulp.watch seems to
+ * think that files are constantly changing.
+ */
+gulp.task( 'watch' , function() {
+	watchr.watch({
+		path: assets,
+		catchupDelay: 250,
+		listeners: {
+			change: function( type , file , currentStat , previousStat ) {
+				var extension = path.extname( file ),
+					task      = null;
+				
+				if ( '.js'   == extension ) task = 'scripts';
+				if ( '.scss' == extension ) task = 'styles';
+				
+				if ( task ) gulp.start( task );
+			}
+		}
+	})
 	
-	gulp.watch( assets + 'css/**' , function() {
-		gulp.run( 'styles' );
-	});
-	
-	gulp.watch( assets + 'js/**' , function() {
-		gulp.run( 'scripts' );
-	});
+	// gulp.watch( assets + 'css/**/*.scss' , [ 'styles' ] );
+	// gulp.watch( assets + 'js/**/*.js'    , [ 'scripts' ] );
 });
+
+gulp.task( 'default' , [ 'styles' , 'scripts' , 'watch' ] );
